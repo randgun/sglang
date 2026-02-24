dbeb4b5f79bc3fa2f03ebd0d5f09268f04cbc698
AndyLi429 <124504189@qq.com>
2026-02-24 16:58:55 +0800

    add logs

--- python/sglang/srt/models/qwen2_moe.py
+++ python/sglang/srt/models/qwen2_moe.py
@@ -635,7 +635,7 @@ class Qwen2MoeModel(nn.Module):
             residual = None
             if self.enable_prefill_cp and use_pcp(forward_batch):
                 hidden_states = cp_split_and_rebuild_data(forward_batch, hidden_states)
-                if torch.distributed.get_rank()==0 or torch.distributed.get_rank()==1:
+                if torch.distributed.get_rank() in (0,1,2,3):
                     print(f"cp split and rebuild hidden states,{torch.distributed.get_rank()=},{hidden_states.sum()=},{hidden_states[:,:3]}") 
         else:
             assert pp_proxy_tensors is not None
@@ -645,7 +645,7 @@ class Qwen2MoeModel(nn.Module):
         if self.enable_prefill_cp and use_pcp(forward_batch):
             positions = cp_split_and_rebuild_position(forward_batch, positions)
             if torch.distributed.get_rank() in (0,1,2,3,4,5,6,7):
-                    print(f"cp split and rebuild position,{torch.distributed.get_rank()=},{positions.sum()=},{positions[:]}") 
+                print(f"cp split and rebuild position,{torch.distributed.get_rank()=},{positions.sum()=},{positions[:]}") 
 
         aux_hidden_states = []
         if forward_batch.can_run_tbo:
@@ -678,6 +678,8 @@ class Qwen2MoeModel(nn.Module):
                             else None
                         ),
                     )
+                    if torch.distributed.get_rank() in (0,1,2,3):
+                        print(f"+++ hidden_states and positions,{torch.distributed.get_rank()=},{hidden_states.sum()=},{positions[:]}") 
         if not self.pp_group.is_last_rank:
             return PPProxyTensors(
                 {
