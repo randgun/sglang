1d5cfbad27c708268f2fa00a2bd1b087c8469bdc
Kailong Lu <kelonlu@163.com>
2026-02-27 16:54:32 +0800

    fix

--- python/sglang/srt/models/glm4_moe_nextn.py
+++ python/sglang/srt/models/glm4_moe_nextn.py
@@ -14,6 +14,7 @@
 
 """Inference-only GLM-4.5, GLM-4.6 Speculative Decoding."""
 
+import contextlib
 import logging
 from typing import Iterable, Optional, Tuple
 
@@ -35,6 +36,7 @@ from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 from sglang.srt.models.glm4_moe import Glm4MoeDecoderLayer, Glm4MoeForCausalLM
 from sglang.srt.server_args import get_global_server_args
 from sglang.srt.utils import add_prefix
+from sglang.srt.environ import temp_set_env
 
 logger = logging.getLogger(__name__)
 
@@ -126,7 +128,8 @@ class Glm4MoeForCausalLMNextN(Glm4MoeForCausalLM):
         nn.Module.__init__(self)
         self.config = config
         self.tp_size = get_tensor_model_parallel_world_size()
-        self.quant_config = quant_config
+        self.needs_quant_draft = get_global_server_args().speculative_draft_model_quantization
+        self.quant_config = quant_config if self.needs_quant_draft else None
         self.model = Glm4MoeModelNextN(
             config, quant_config, prefix=add_prefix("model", prefix)
         )
@@ -150,7 +153,19 @@ class Glm4MoeForCausalLMNextN(Glm4MoeForCausalLM):
         positions: torch.Tensor,
         forward_batch: ForwardBatch,
     ) -> torch.Tensor:
-        hidden_states = self.model(input_ids, positions, forward_batch)
+        # Support unquant speculative draft model
+        if self.needs_quant_draft:
+            cxt = contextlib.nullcontext()
+        else:
+            unquant_patch = {
+                'SGLANG_DEEPEP_BF16_DISPATCH': '1',
+                'DEEP_NORMAL_MODE_USE_INT8_QUANT': '0'
+            }
+            cxt = temp_set_env(allow_sglang=True, **unquant_patch)
+
+        with cxt:
+            hidden_states = self.model(input_ids, positions, forward_batch)
+
         return self.logits_processor(
             input_ids, hidden_states, self.lm_head, forward_batch
         )
