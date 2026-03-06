8ff90fd2c78412c08d66c3a1795a418f2232a8f0
Kailong Lu <kelonlu@163.com>
2026-03-06 17:16:09 +0800

    [NPU] optimize glm4.7

--- python/sglang/srt/layers/quantization/modelslim/modelslim.py
+++ python/sglang/srt/layers/quantization/modelslim/modelslim.py
@@ -57,13 +57,13 @@ def npu_wrapper_rmsnorm_forward(func):
         residual: Optional[torch.Tensor] = None,
         post_residual_addition: Optional[torch.Tensor] = None,
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
-        from sgl_kernel_npu.norm.add_rmsnorm_bias import add_rmsnorm_bias
-
         if not x.is_contiguous():
             x = x.contiguous()
         if residual is not None:
             if post_residual_addition is not None:
                 residual = residual + post_residual_addition
+            from sgl_kernel_npu.norm.add_rmsnorm_bias import add_rmsnorm_bias
+
             out, residual_out = add_rmsnorm_bias(
                 x,
                 residual,
@@ -72,9 +72,14 @@ def npu_wrapper_rmsnorm_forward(func):
                 self.variance_epsilon,
             )
             return out.to(x.dtype), residual_out
+        from sgl_kernel_npu.norm.rmsnorm_bias import rmsnorm_bias
 
-        out = torch.ops.npu.npu_rms_norm(x, self.weight.data, self.variance_epsilon)[0]
-        out = out + self.bias
+        out = rmsnorm_bias(
+            x,
+            self.weight.data,
+            self.bias,
+            self.variance_epsilon,
+        )
         return out.to(x.dtype)
 
     return _rmsnorm_forward_oot
