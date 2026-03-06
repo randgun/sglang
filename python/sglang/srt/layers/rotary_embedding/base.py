5c06eab555f1551368d7a79b88d97965261c683b
cen121212 <ershoucen@163.com>
2026-03-06 19:44:47 +0800

    glm5 optimize (#71)
    * solve conflict
    
    * solve conflict
    
    * glm5 optimize
    
    * adapt mlp no quant
    
    ---------
    
    Co-authored-by: cen121212 <luochen23@huawei.com>

--- python/sglang/srt/layers/rotary_embedding/base.py
+++ python/sglang/srt/layers/rotary_embedding/base.py
@@ -37,6 +37,7 @@ if _is_cuda:
 
 if _is_npu:
     import torch_npu
+    from sgl_kernel_npu.norm.fused_rope_qk_mqa import fused_rope_qk_mqa
 
 
 class RotaryEmbedding(MultiPlatformOp):
@@ -199,6 +200,7 @@ class RotaryEmbedding(MultiPlatformOp):
 
         if offsets is not None:
             positions = positions + offsets
+
         positions = positions.flatten()
         num_tokens = positions.shape[0]
         cos_sin = self.cos_sin_cache.index_select(0, positions)
@@ -233,8 +235,21 @@ class RotaryEmbedding(MultiPlatformOp):
         assert (
             fused_set_kv_buffer_arg is None
         ), "fused_set_kv_buffer_arg is not supported for npu implementation"
-        if query.dtype == torch.bfloat16 and self.cos_sin_cache.dtype == torch.float:
-            return self.forward_native(positions, query, key, offsets)
+        if (
+            query.dtype == torch.bfloat16
+            and self.cos_sin_cache.dtype == torch.float
+            or key.ndim == 3
+        ):
+            if query.shape[0] * query.shape[1] < 65535:
+                return fused_rope_qk_mqa(
+                    query,
+                    key,
+                    self.cos_sin_cache.index_select(0, positions),
+                    self.rotary_dim,
+                    self.is_neox_style,
+                )
+            else:
+                return self.forward_native(positions, query, key, offsets)
         if self.is_neox_style:
             rotary_mode = "half"
         else:
--- python/sglang/srt/models/deepseek_nextn.py
+++ python/sglang/srt/models/deepseek_nextn.py
@@ -17,6 +17,7 @@
 import logging
 from typing import Iterable, Optional, Tuple
 
+import os
 import torch
 from torch import nn
 from transformers import PretrainedConfig
@@ -137,6 +138,8 @@ class DeepseekModelNextN(nn.Module):
         forward_batch: ForwardBatch,
         input_embeds: torch.Tensor = None,
     ) -> torch.Tensor:
+        os.environ["SGLANG_DEEPEP_BF16_DISPATCH"] = "1"
+        os.environ["DEEP_NORMAL_MODE_USE_INT8_QUANT"] = "0"
         zero_allocator = BumpAllocator(
             buffer_size=2,
             dtype=torch.float32,
@@ -189,6 +192,8 @@ class DeepseekModelNextN(nn.Module):
                     torch.cuda.current_stream(),
                 )
 
+        os.environ["SGLANG_DEEPEP_BF16_DISPATCH"] = "0"
+        os.environ["DEEP_NORMAL_MODE_USE_INT8_QUANT"] = "1"
         return hidden_states
 
 
@@ -212,6 +217,7 @@ class DeepseekV3ForCausalLMNextN(DeepseekV3ForCausalLM):
         nn.Module.__init__(self)
         self.config = config
         self.tp_size = get_tensor_model_parallel_world_size()
+        quant_config = None
         self.quant_config = quant_config
         # if not set, model load will be broken in DeepseekV3ForCausalLM load_weights()
         self.pp_group = get_pp_group()
