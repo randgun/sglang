Liwansi/sglang_vans

[feat] draft_prefetch

python/sglang/srt/model_executor/forward_batch_info.py
@@ -739,14 +739,15 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
 
         self.original_global_num_tokens_cpu = batch.global_num_tokens
         self.global_num_tokens_cpu = global_num_tokens
+        pin_memory = is_pin_memory_available(device)
         self.global_num_tokens_gpu = torch.tensor(
-            global_num_tokens, dtype=torch.int64, pin_memory=is_pin_memory_available(device)
+            global_num_tokens, dtype=torch.int64, pin_memory=pin_memory
         ).to(device, non_blocking=True)
         self.global_num_tokens_for_logprob_cpu = global_num_tokens_for_logprob
         self.global_num_tokens_for_logprob_gpu = torch.tensor(
             global_num_tokens_for_logprob,
             dtype=torch.int64,
-            pin_memory=is_pin_memory_available(device),
+            pin_memory=pin_memory,
         ).to(device, non_blocking=True)
         self.can_run_dp_cuda_graph = batch.can_run_dp_cuda_graph
 
@@ -858,7 +859,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         ret._maybe_init_non_generation_fields(batch)
 
         device = model_runner.device
-        _pin = is_pin_memory_available(device)
+        pin_memory = is_pin_memory_available()
 
         if envs.SGLANG_KV_CANARY_ENABLE_TOKEN_ORACLE.get():
             hashed = _hash_rids_to_tensor(
@@ -887,7 +888,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         num_tokens = len(batch.input_ids) if batch.input_ids is not None else 0
         if enable_num_token_non_padded():
             ret.num_token_non_padded = torch.tensor(
-                num_tokens, dtype=torch.int32, pin_memory=_pin
+                num_tokens, dtype=torch.int32, pin_memory=pin_memory
             ).to(device, non_blocking=True)
         ret.num_token_non_padded_cpu = num_tokens
 
@@ -911,6 +912,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
                     for i in range(block_offset, block_offset + block_size)
                 ],
                 dtype=positions_dtype,
+                pin_memory=pin_memory,
             ).to(device, non_blocking=True)
         elif (
             ret.spec_info is not None
@@ -926,7 +928,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
             if isinstance(extend_seq_lens, list):
                 # Main path: H2D from host lists; populate *_cpu mirrors.
                 assert isinstance(extend_prefix_lens, list)
-                prefill_pin = _pin or (
+                prefill_pin = pin_memory or (
                         _is_npu and str(device).split(":", 1)[0] == "npu"
                 )
                 ret.extend_seq_lens = torch.tensor(
@@ -1442,8 +1444,13 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         # padding
         self._pad_inputs_to_size(model_runner, num_tokens, bs)
         self.global_num_tokens_cpu = global_num_tokens
-        global_num_tokens_pinned = torch.tensor(global_num_tokens, pin_memory=True)
-        self.global_num_tokens_gpu.copy_(global_num_tokens_pinned, non_blocking=True)
+        pin_memory = is_pin_memory_available()
+        global_num_tokens_pinned = torch.tensor(
+            global_num_tokens, pin_memory=pin_memory
+        )
+        self.global_num_tokens_gpu.copy_(
+            global_num_tokens_pinned, non_blocking=pin_memory
+        )
 
         TboForwardBatchPreparer.prepare(
             batch=self, is_draft_worker=model_runner.is_draft_worker

python/sglang/srt/speculative/eagle_disaggregation.py
@@ -4,14 +4,12 @@ from typing import TYPE_CHECKING
 
 import torch
 
-from sglang.srt.environ import envs
 from sglang.srt.layers.attention.dsa.utils import (
     should_remap_pd_dsa_seed_to_local_slots,
 )
 from sglang.srt.managers.overlap_utils import RelayPayload
 from sglang.srt.model_executor.forward_batch_info import CaptureHiddenMode
 from sglang.srt.speculative.eagle_info import EagleDraftInput
-from sglang.srt.speculative.eagle_zero_bubble import pad_zero_bubble_seed
 
 if TYPE_CHECKING:
     from sglang.srt.managers.overlap_utils import FutureMap
@@ -27,6 +25,16 @@ def build_eagle_disagg_draft_input(
 ) -> EagleDraftInput:
     num_states = server_args.speculative_eagle_topk
     if server_args.enable_multi_layer_eagle:
+        num_states *= server_args.speculative_num_steps
+    elif (
+        server_args.enable_draft_prefetch and server_args.speculative_num_steps > 1
+    ):
+        # Draft-prefetch: the prefill engine pre-concatenated the seed to the
+        # candidate width (steps * topk) and normalized it to target-vocab ids
+        # (see _pad_topk_for_draft_prefetch). Keep the full width here -- the
+        # first decode round's prepare_verify_fully_async_decoding runs
+        # torch.topk(num_draft_tokens - 1) over it, which crashes on a
+        # width-1 seed and merge_batch needs matching widths anyway.
         num_states *= server_args.speculative_num_steps
 
     topk_p = torch.stack(
@@ -51,13 +59,6 @@ def build_eagle_disagg_draft_input(
         ],
         dim=0,
     )
-    if envs.SGLANG_SPEC_V2_ZERO_BUBBLE.get():
-        topk_p, topk_index = pad_zero_bubble_seed(
-            topk_p=topk_p,
-            topk_index=topk_index,
-            num_steps=server_args.speculative_num_steps,
-            topk=server_args.speculative_eagle_topk,
-        )
 
     hidden_states = torch.stack(
         [req.hidden_states_tensor for req in batch.reqs], dim=0

randgun/sglang

python/sglang/srt/model_executor/forward_batch_info.py
@@ -826,6 +826,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         ret._maybe_init_non_generation_fields(batch)
 
         device = model_runner.device
+        pin_memory = is_pin_memory_available()
 
         if envs.SGLANG_KV_CANARY_ENABLE_TOKEN_ORACLE.get():
             hashed = _hash_rids_to_tensor(
@@ -854,9 +855,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         num_tokens = len(batch.input_ids) if batch.input_ids is not None else 0
         if enable_num_token_non_padded():
             ret.num_token_non_padded = torch.tensor(
-                num_tokens,
-                dtype=torch.int32,
-                pin_memory=is_pin_memory_available(device),
+                num_tokens, dtype=torch.int32, pin_memory=pin_memory
             ).to(device, non_blocking=True)
         ret.num_token_non_padded_cpu = num_tokens
 
@@ -880,6 +879,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
                     for i in range(block_offset, block_offset + block_size)
                 ],
                 dtype=positions_dtype,
+                pin_memory=pin_memory,
             ).to(device, non_blocking=True)
         elif (
             ret.spec_info is not None
@@ -895,12 +895,14 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
             if isinstance(extend_seq_lens, list):
                 # Main path: H2D from host lists; populate *_cpu mirrors.
                 assert isinstance(extend_prefix_lens, list)
-                pin_memory = is_pin_memory_available(device)
+                prefill_pin = pin_memory or (
+                        _is_npu and str(device).split(":", 1)[0] == "npu"
+                )
                 ret.extend_seq_lens = torch.tensor(
-                    extend_seq_lens, dtype=torch.int32, pin_memory=pin_memory
+                    extend_seq_lens, dtype=torch.int32, pin_memory=prefill_pin
                 ).to(device, non_blocking=True)
                 ret.extend_prefix_lens = torch.tensor(
-                    extend_prefix_lens, dtype=torch.int32, pin_memory=pin_memory
+                    extend_prefix_lens, dtype=torch.int32, pin_memory=prefill_pin
                 ).to(device, non_blocking=True)
                 ret.extend_prefix_lens_cpu = extend_prefix_lens
                 ret.extend_seq_lens_cpu = extend_seq_lens
@@ -1486,12 +1488,12 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
         # padding
         self._pad_inputs_to_size(model_runner, num_tokens, bs)
         self.global_num_tokens_cpu = global_num_tokens
-        self.use_pin_memory = not _is_cpu
+        pin_memory = is_pin_memory_available()
         global_num_tokens_pinned = torch.tensor(
-            global_num_tokens, pin_memory=self.use_pin_memory
+            global_num_tokens, pin_memory=pin_memory
         )
         self.global_num_tokens_gpu.copy_(
-            global_num_tokens_pinned, non_blocking=self.use_pin_memory
+            global_num_tokens_pinned, non_blocking=pin_memory
         )
 
         TboForwardBatchPreparer.prepare(

python/sglang/srt/speculative/eagle_disaggregation.py
@@ -28,6 +28,16 @@ def build_eagle_disagg_draft_input(
     num_states = spec.speculative_eagle_topk
     if spec.enable_multi_layer_eagle:
         num_states *= spec.speculative_num_steps
+    elif (
+        spec.enable_draft_prefetch and spec.speculative_num_steps > 1
+    ):
+        # Draft-prefetch: the prefill engine pre-concatenated the seed to the
+        # candidate width (steps * topk) and normalized it to target-vocab ids
+        # (see _pad_topk_for_draft_prefetch). Keep the full width here -- the
+        # first decode round's prepare_verify_fully_async_decoding runs
+        # torch.topk(num_draft_tokens - 1) over it, which crashes on a
+        # width-1 seed and merge_batch needs matching widths anyway.
+        num_states *= spec.speculative_num_steps
 
     topk_p = torch.stack(
         [